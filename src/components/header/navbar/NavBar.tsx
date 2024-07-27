d8100e89a174af7e46b446e7d2afbedaadf94860
IScetta <150315667+IScetta@users.noreply.github.com>
2024-07-27 00:33:18 -0300

    Act 6

--- src/components/header/navbar/NavBar.tsx
+++ src/components/header/navbar/NavBar.tsx
@@ -19,18 +19,18 @@ const scrollToSection = (id:string) => {
 
     return(
         <div className="fixed top-0 right-0 w-full z-50 ">
-        <div className=" flex flex-row justify-end p-4 w-screen bg-blueDark "> 
+        <div className=" flex flex-row justify-end p-2 w-screen bg-blueDark "> 
         <div className=" flex flex-row items-center mr-32">
            {title.map( (item, index)=>{
             return(
                 <div className=" mx-2 p-2" key={index}>
-                <button onClick={() => scrollToSection(item)} className="hover:border-b-4 border-offRed">{item}</button>
+                <button onClick={() => scrollToSection(item)} className="text-[12px]  border-b-4 border-blueDark hover:border-offRed">{item}</button>
                 </div>
             )
         })
 
             }
-            <Link href={""} className="flex items-center p-2 border-4 border-white rounded-md hover:border-offRed">CV</Link>
+            <Link href={""} className="flex items-center p-2 text-[12px] border-4 border-white rounded-md hover:border-offRed">CV</Link>
         </div> 
         </div>
         
